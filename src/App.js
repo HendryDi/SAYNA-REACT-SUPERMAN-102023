9338436344acc3c0e6b5a332ff3e29a22c61de25
HendryDi <h9ndri@gmail.com>
2023-10-18 09:07:20 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from './pages/Home';
+import Game from './pages/Game';
+import Eshop from './pages/Eshop';
+import Compte from './pages/Compte';
+
+
+function App() {
+  return (
+  <Router>
+    <Routes>
+      <Route path='/' element={ <Home/> }/>
+      <Route path='/game' element={ <Game/> }/>
+      <Route path='/eshop' element={ <Eshop/> }/>
+      <Route path='/compte' element={ <Compte/> }/>
+    </Routes>
+  </Router>
+  );
+}
+
+export default App;
